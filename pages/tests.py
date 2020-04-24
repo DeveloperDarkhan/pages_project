be10610a7cde34a81e62071e834ece510f1571c4
DeveloperDarkhan <dmuhamedjarov@gmail.com>
2020-04-24 16:12:02 +0600

    Changed test.py and left comment about previous test

--- pages/tests.py
+++ pages/tests.py
@@ -1,14 +1,45 @@
 from django.test import TestCase, SimpleTestCase
-
+from django.urls import reverse
 # Create your tests here.
-class SimpleTests(SimpleTestCase):
 
-    def test_home_page_view_status_code(self):
+# class SimpleTests(SimpleTestCase):
+
+#     def test_home_page_view_status_code(self):
+#         request = self.client.get('/')
+#         self.assertEqual(request.status_code, 200)
+
+
+#     def test_about_page_view_code(self):
+
+#         request = self.client.get('/about/')
+#         self.assertEqual(request.status_code, 200)
+
+class HomePageTest(TestCase):
+
+    def test_view_url_exists_at_proper_location(self):
         request = self.client.get('/')
         self.assertEqual(request.status_code, 200)
 
+    def test_view_url_by_name(self):
+        request = self.client.get(reverse('home'))
+        self.assertEqual(request.status_code, 200)
 
-    def test_about_page_view_code(self):
+    def test_view_uses_correct_template(self):
+        request = self.client.get(reverse('home'))
+        self.assertEqual(request.status_code, 200)
+        self.assertTemplateUsed(request, 'home.html')
 
+class AboutPageTest(TestCase):
+
+    def test_view_url_exists_at_proper_location(self):
         request = self.client.get('/about/')
         self.assertEqual(request.status_code, 200)
+
+    def test_view_url_by_name(self):
+        request = self.client.get(reverse('about'))
+        self.assertEqual(request.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        request = self.client.get(reverse('about'))
+        self.assertEqual(request.status_code, 200)
+        self.assertTemplateUsed(request, 'about.html')
